1220dcb48b850ff5a4464f3c586032ead04931b6
shmuel <shmuel.disraeli@gmail.com>
2022-11-21 14:10:58 +0200

    remove dependency on dex_liquidity.json + remove asset name mappings + remove configurations around it

--- src/pages/Liquidity.js
+++ src/pages/Liquidity.js
@@ -57,27 +57,21 @@ const columns = [
 
 class Liquidity extends Component {
   render (){
-    const loading = mainStore['dex_liquidity_loading']
+    const loading = mainStore['usd_volume_for_slippage_data']
     const assets = {}
-    const rawData = Object.assign({}, mainStore['dex_liquidity_data'] || {})
+    const rawData = Object.assign({}, mainStore['usd_volume_for_slippage_data'] || {})
     const {json_time} = rawData
 
     if(json_time){
       delete rawData.json_time
     }
+
     Object.entries(rawData).forEach(([k, v])=> {
       const asset = k.split('-')[0]
       assets[asset] = assets[asset] || { name: asset, lps: []}
       v.key = k
       assets[asset].lps.push(v)
     })
-    
-    if(window.APP_CONFIG.PLATFORM_ID === '0'){
-      assets['WNEAR'] = { name: 'WNEAR', lps: []}
-    }    
-    if(window.APP_CONFIG.PLATFORM_ID === '2'){
-      assets['gOHM'] = { name: 'gOHM', lps: []}
-    }
 
     return (
       <div>
@@ -90,12 +84,6 @@ class Liquidity extends Component {
               <SlippageChart data={asset.name} i={i}/>
             </div>
             <div style={{marginLeft: '30px'}}>
-            {window.APP_CONFIG.feature_flags.dexLiquidityLpStatsRow && !!asset.lps.length && <Box>
-                   <DataTable
-                    columns={columns}
-                    data={asset.lps}
-                  />
-              </Box>}
             </div>
           </details>)}
         </Box>
