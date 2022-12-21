5fd833372f8a37b62c33bd2760ccab567464b2ef
Alfulinku <9356022+alfulinku@users.noreply.github.com>
2022-12-21 14:16:53 +0100

    reset aurigami.js

--- public/configs/aurigami.js
+++ public/configs/aurigami.js
@@ -6,49 +6,44 @@ window.APP_CONFIG = {
     WHITE_LOGO: 'aurigami.svg',
     BLACK_LOGO: 'aurigami.svg',
     feature_flags: {
-        alerts: false,
-        loopingToggle: true,
-        computeReverseSandbox: true
+      alerts: false,
+      loopingToggle: true
     },
     apiEndpoints: [
-        'overview',
-        'accounts',
-        'oracles',
-        'usd_volume_for_slippage',
-        'current_simulation_risk',
-        'risk_params',
-        'lending_platform_current',
-        'whale_accounts',
-        'open_liquidations',
+      'overview', 'accounts', 
+      'oracles', 'usd_volume_for_slippage', 
+      'current_simulation_risk',
+      'risk_params', 'lending_platform_current', 'whale_accounts', 
+      'open_liquidations',
     ],
     SECTIONS: [
-        {
-            name: 'system-status',
-            defaultVisible: true,
-        },
-        {
-            name: 'overview',
-            defaultVisible: true,
-        },
-        {
-            name: 'collateral-factors',
-        },
-        {
-            name: 'sandbox',
-        },
-        {
-            name: 'asset-distribution',
-            defaultVisible: true,
-        },
-        {
-            name: 'open-liquidations',
-        },
-        {
-            name: 'oracle-deviation',
-        },
-        {
-            name: 'liquidity',
-            displayName: 'DEX Liquidity',
-        },
+      {
+        name: 'system-status',
+        defaultVisible: true,
+      },    
+      {
+        name: 'overview',
+        defaultVisible: true,
+      },   
+      {
+        name: 'collateral-factors',
+      },    
+      {
+        name: 'sandbox',
+      },    
+      {
+        name: 'asset-distribution',
+        defaultVisible: true,
+      },
+      {
+        name: 'open-liquidations',
+      },
+      {
+        name: 'oracle-deviation',
+      },
+      {
+        name: 'liquidity',
+        displayName: 'DEX Liquidity'
+      },
     ],
-}
+  }
